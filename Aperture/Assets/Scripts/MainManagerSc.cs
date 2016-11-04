f6385b20bdb73fa5ce35d7755fa60273e7960371
Laurent <laurent.cetin@telecom-sudparis.eu>
2016-11-05 00:46:21 +0100

    PrimaryManager
    Main Manager avec GameManager

---
+++ Aperture/Assets/Scripts/MainManagerSc.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainManagerSc : MonoBehaviour
+{
+    public GameObject gameManager;          //GameManager prefab to instantiate.
+    //public GameObject soundManager;         //SoundManager prefab to instantiate.
+
+
+    void Awake()
+    {
+        //Check if a GameManager has already been assigned to static variable GameManager.instance or if it's still null
+        if (GameManagerSc.instance == null)
+
+            //Instantiate gameManager prefab
+            gameManager = (GameObject)Instantiate(gameManager);
+
+        //Check if a SoundManager has already been assigned to static variable GameManager.instance or if it's still null
+        // if (SoundManager.instance == null)
+
+        //Instantiate SoundManager prefab
+        //Instantiate(soundManager);
+    }
+}
