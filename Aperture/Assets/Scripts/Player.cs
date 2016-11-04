70486aafed3bc2b13f1dcf557f730a639576de24
Meneran <guillaume.chabbert@telecom-sudparis.eu>
2016-11-05 00:42:17 +0100

    patch movement: continue

--- Aperture/Assets/Scripts/Player.cs
+++ Aperture/Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ public class Player : MonoBehaviour
 {
 
     private Vector3 stop = new Vector3(0, 0, 0);
+    private Vector3 move;
+    private Vector3 newPos;
     private bool horizontal, vertical;
     [SerializeField]
     private float tileSize;
@@ -12,6 +14,8 @@ public class Player : MonoBehaviour
     private float saveTimer;
     private float timer;
     // Use this for initialization
+
+
     void Start()
     {
         timer = saveTimer;
@@ -27,24 +31,35 @@ public class Player : MonoBehaviour
             if (Input.GetButton("Right"))
             {
                 transform.localEulerAngles = (new Vector3(0, 0, 0));
-                transform.position += new Vector3(1, 0, 0);
+                newPos = transform.position + new Vector3(1, 0, 0) * tileSize;
+                move = new Vector3(1, 0, 0) * tileSize / 10;
+                //transform.position += new Vector3(1, 0, 0) * tileSize;
             }
             if (Input.GetButton("Left"))
             {
                 transform.localEulerAngles = (new Vector3(0, 0, 180));
-                transform.position += new Vector3(-1, 0, 0);
+                newPos = transform.position + new Vector3(-1, 0, 0) * tileSize;
+                move = new Vector3(-1, 0, 0) * tileSize / 10;
+                //transform.position += new Vector3(-1, 0, 0) * tileSize;
             }
             if (Input.GetButton("Up"))
             {
                 transform.localEulerAngles = (new Vector3(0, 0, 90));
-                transform.position += new Vector3(0, 1, 0);
+                newPos = transform.position + new Vector3(0, 1, 0) * tileSize;
+                move = new Vector3(0, 1, 0) * tileSize / 10;
+                //transform.position += new Vector3(0, 1, 0) * tileSize;
             }
             if (Input.GetButton("Down"))
             {
                 transform.localEulerAngles = (new Vector3(0, 0, -90));
-                transform.position += new Vector3(0, -1, 0);
+                newPos = transform.position + new Vector3(0, -1, 0) * tileSize;
+                move = new Vector3(0, -1, 0) * tileSize / 10;
+                //transform.position += new Vector3(0, -1, 0) * tileSize;
             }
         }
-
+        if (transform.position != newPos)
+        {
+            transform.position += move;
+        }
     }
 }
