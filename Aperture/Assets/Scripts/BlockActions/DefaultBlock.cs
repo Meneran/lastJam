a6c74ba38c40c7b2f8bb7fd71f074d4a6088f386
Victor Rouquette <victor.rouquette@orange.fr>
2016-11-05 00:42:13 +0100

    Blocks modification

---
+++ Aperture/Assets/Scripts/BlockActions/DefaultBlock.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DefaultBlock : MonoBehaviour {
+
+    [SerializeField]
+    private bool allowToPass;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void enablePassing()
+    {
+        allowToPass = true;
+    }
+
+    public void disablePassing()
+    {
+        allowToPass = false;
+    }
+
+    public bool getPassing()
+    {
+        return allowToPass;
+    }
+}
