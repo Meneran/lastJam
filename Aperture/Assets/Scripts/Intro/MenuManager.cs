14acf3e047aed84f9b7885ac0a597f69a7a708a5
Herobrine <hugoattal@hotmail.fr>
2016-11-05 18:04:05 +0100

    Added Level Selector

--- Aperture/Assets/Scripts/Intro/MenuManager.cs
+++ Aperture/Assets/Scripts/Intro/MenuManager.cs
@@ -1,40 +1,99 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum StateMenu
+{
+	Start,
+	LevelSelector,
+	Options
+};
+
 public class MenuManager : MonoBehaviour {
 
 	public int selectedMenu;
 
+	public float timer;
+	private float clock;
+
 	public GameObject[] btn_object;
+	public GameObject[] lvl_object;
+	public GameObject fullHolder;
+	
+	private GameObject[] std_object;
 	private int nBtn;
+	private StateMenu state;
 
 	// Use this for initialization
 	void Start ()
 	{
-		nBtn = btn_object.Length;
-		OnSelect(btn_object[selectedMenu]);
+		state = StateMenu.Start;
+
+		Init(btn_object);
+	}
+
+	void Init(GameObject[] cObject)
+	{
+		std_object = cObject;
+		selectedMenu = 0;
+
+		nBtn = std_object.Length;
+		OnSelect(std_object[selectedMenu]);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		float displacement = -2.5f;
+
+		if (clock > 0)
+		{
+			clock -= Time.deltaTime;
+
+			if (state == StateMenu.Start)
+			{
+				fullHolder.transform.position = new Vector3(sinTransition(clock/timer) * displacement, 0, 0);
+			}
+			else if (state == StateMenu.LevelSelector)
+			{
+				fullHolder.transform.position = new Vector3(sinTransition(1 - clock / timer) * displacement, 0, 0);
+			}
+		}
+		else
+		{
+			clock = 0;
+			if (state == StateMenu.Start)
+			{
+				fullHolder.transform.position = new Vector3(0, 0, 0);
+			}
+			else if (state == StateMenu.LevelSelector)
+			{
+				fullHolder.transform.position = new Vector3(displacement, 0, 0);
+			}
+		}
+
 		if (Input.GetKeyDown(KeyCode.DownArrow))
 		{
-			OnLeave(btn_object[selectedMenu]);
+			OnLeave(std_object[selectedMenu]);
 			selectedMenu = (selectedMenu + 1) % nBtn;
-			OnSelect(btn_object[selectedMenu]);
+			OnSelect(std_object[selectedMenu]);
 		}
 
 		if (Input.GetKeyDown(KeyCode.UpArrow))
 		{
-			OnLeave(btn_object[selectedMenu]);
+			OnLeave(std_object[selectedMenu]);
 			selectedMenu = (selectedMenu + nBtn - 1) % nBtn;
-			OnSelect(btn_object[selectedMenu]);
+			OnSelect(std_object[selectedMenu]);
 		}
 
 		if (Input.GetKeyDown(KeyCode.Return))
 		{
+			OnLeave(std_object[selectedMenu]);
 			ExecuteAction();
+		}
+
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			ExecuteAction(0);
 		}
 	}
 
@@ -48,12 +107,35 @@ public class MenuManager : MonoBehaviour {
 		btn_object.GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f);
 	}
 
-	void ExecuteAction()
+	void ExecuteAction(int type = -1)
 	{
-		if (selectedMenu == 2)
+		if ((type == 0) && (state != StateMenu.Start))
 		{
-			Debug.Log("Exiting...");
-			Application.Quit();
+			OnLeave(std_object[selectedMenu]);
+			clock = timer;
+			state = StateMenu.Start;
+			Init(btn_object);
 		}
+		else if (state == StateMenu.Start)
+		{
+			switch(selectedMenu)
+			{
+				case 0:
+					Debug.Log("Goto Level Selector");
+					clock = timer;
+					state = StateMenu.LevelSelector;
+					Init(lvl_object);
+					break;
+				case 2:
+					Debug.Log("Exiting...");
+					Application.Quit();
+					break;
+			}
+		}
+	}
+
+	float sinTransition(float x)
+	{
+		return 0.5f - 0.5f * Mathf.Cos(x * Mathf.PI);
 	}
 }
