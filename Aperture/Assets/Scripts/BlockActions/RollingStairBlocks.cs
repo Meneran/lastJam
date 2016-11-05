45bae8f93d818117ae2c759847ce3e71c3ece95a
Victor Rouquette <victor.rouquette@orange.fr>
2016-11-05 21:28:07 +0100

    Rolling stairs update

--- Aperture/Assets/Scripts/BlockActions/RollingStairBlocks.cs
+++ Aperture/Assets/Scripts/BlockActions/RollingStairBlocks.cs
@@ -3,9 +3,29 @@ using System.Collections;
 
 public class RollingStairBlocks : DefaultBlock {
 
+    [SerializeField]
+    private Direction direction;
+
+    private bool isActive;
+
 	// Use this for initialization
 	void Start () {
-	
+        Vector3 Zaxis = new Vector3(0, 0, 1);
+        switch (direction)
+        {
+            case Direction.UP:
+                transform.Rotate(Zaxis, -90);
+                break;
+            case Direction.DOWN:
+                transform.Rotate(Zaxis, 90);
+                break;
+            case Direction.LEFT:
+                transform.Rotate(Zaxis, 0f);
+                break;
+            case Direction.RIGHT:
+                transform.Rotate(Zaxis, 180);
+                break;
+        }
 	}
 	
 	// Update is called once per frame
@@ -15,6 +35,64 @@ public class RollingStairBlocks : DefaultBlock {
 
     public override void activateOnWalk(GameObject player)
     {
-        //player.GetComponent<>
+        if (player.CompareTag("player1"))
+        {
+            switch (direction)
+            {
+                case Direction.UP:
+                    InputManagerSc.instance.upP1 = true;
+                    break;
+                case Direction.DOWN:
+                    InputManagerSc.instance.downP1 = true;
+                    break;
+                case Direction.RIGHT:
+                    InputManagerSc.instance.rightP1 = true;
+                    break;
+                case Direction.LEFT:
+                    InputManagerSc.instance.leftP1 = true;
+                    break;
+            }
+        }
+        else if (player.CompareTag("player2"))
+        {
+            switch (direction)
+            {
+                case Direction.UP:
+                    InputManagerSc.instance.upP2 = true;
+                    break;
+                case Direction.DOWN:
+                    InputManagerSc.instance.downP2 = true;
+                    break;
+                case Direction.RIGHT:
+                    InputManagerSc.instance.rightP2 = true;
+                    break;
+                case Direction.LEFT:
+                    InputManagerSc.instance.leftP2 = true;
+                    break;
+            }
+        }
+    }
+
+    public override void activate()
+    {
+        GetComponent<Animator>().SetBool("activated", true);
+        isActive = true;
+    }
+
+    public override void desactivate()
+    {
+        GetComponent<Animator>().SetBool("activated", true);
+        isActive = false;
+    }
+
+    public override void changeState()
+    {
+        if (isActive)
+        {
+            activate();
+        } else
+        {
+            desactivate();
+        }
     }
 }
