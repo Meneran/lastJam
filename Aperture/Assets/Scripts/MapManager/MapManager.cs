a02b04fad95c86300a837563bbaaae32b849d28d
Victor Rouquette <victor.rouquette@orange.fr>
2016-11-05 19:16:27 +0100

    correction on MapManager

--- Aperture/Assets/Scripts/MapManager/MapManager.cs
+++ Aperture/Assets/Scripts/MapManager/MapManager.cs
@@ -44,7 +44,7 @@ public class MapManager : Singleton<MapManager>
 
 	public Vector2 GetTileCoord(Vector2 coord)
 	{
-		return new Vector2((int)(coord.x / 0.16f), (int)(coord.y / 0.16f));
+		return new Vector2((int)Mathf.Round(coord.x / 0.16f), (int)Mathf.Round(coord.y / 0.16f));
 	}
 
 	void DisplayMap()
