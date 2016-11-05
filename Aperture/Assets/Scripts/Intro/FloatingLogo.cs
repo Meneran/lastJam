b46a089fe96f65077498ec277ab0183bb67634cd
Herobrine <hugoattal@hotmail.fr>
2016-11-05 15:32:15 +0100

    Added Intro Scene

---
+++ Aperture/Assets/Scripts/Intro/FloatingLogo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingLogo : MonoBehaviour {
+
+	public Vector3 startPosition;
+	public float floatingSpeed;
+	public float floatingDelta;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = this.gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float offset = Time.time * floatingSpeed;
+		this.gameObject.transform.position = startPosition + new Vector3(0, Mathf.Cos(offset) * floatingDelta, 0);
+	}
+}
