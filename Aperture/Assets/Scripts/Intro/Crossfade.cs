68effdde42dff908af6cfe68d0364b998e6dd6a1
Herobrine <hugoattal@hotmail.fr>
2016-11-06 10:28:46 +0100

    Added Crossfade

---
+++ Aperture/Assets/Scripts/Intro/Crossfade.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crossfade : MonoBehaviour
+{
+	public float timer;
+	private float clock;
+
+	public bool exitState;
+
+	// Use this for initialization
+	void Start ()
+	{
+		clock = timer;
+		exitState = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (clock > 0)
+		{
+			clock -= Time.deltaTime;
+		}
+		else
+		{
+			clock = 0;
+		}
+
+		if (exitState)
+		{
+			this.gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1 - sinTransition(clock / timer));
+		}
+		else
+		{
+			this.gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, sinTransition(clock/timer));
+		}
+	}
+
+	public void changeState()
+	{
+		exitState = true;
+		float clock = timer;
+	}
+
+	float sinTransition(float x)
+	{
+		return 0.5f - 0.5f * Mathf.Cos(x * Mathf.PI);
+	}
+}
